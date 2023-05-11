1daee2363f4ad4d2a7f6251d87e1f07acfe721ed
Davefxy <davefxy@yahoo.com>
2023-05-11 12:54:07 -0600

    Make Microcharts work with Maui nugets

--- MicrochartsControl/Constants/DefaultValues.cs
+++
@@ -1,9 +0,0 @@
-namespace Microcharts
-{
-    internal static class DefaultValues
-    {
-        internal const byte BarAreaAlpha = 32;
-        internal const float MinBarHeight = 4;
-        internal const float CornerRadius = 0;
-    }
-}
